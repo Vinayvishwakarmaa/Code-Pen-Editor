670a670a72c522f4fdec26abe24654170b7fd056
vinayVishwakarmaa <vinaykumar843569@gmail.com>
2024-04-12 22:05:38 +0530

    first commit

---
+++ src/context/DataProvider.jsx
@@ -0,0 +1,28 @@
+import { createContext } from "react";
+import React from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
+export const DataContext = createContext(null);
+
+const DataProvider = ({ children }) => {
+  const [html, setHtml] = useLocalStorage("html", "");
+  const [css, setCss] = useLocalStorage("css", "");
+  const [js, setJs] = useLocalStorage("js", "");
+  return (
+    <>
+      <DataContext.Provider
+        value={{
+          html,
+          setHtml,
+          css,
+          setCss,
+          js,
+          setJs,
+        }}
+      >
+        {children}
+      </DataContext.Provider>
+    </>
+  );
+};
+
+export default DataProvider;
